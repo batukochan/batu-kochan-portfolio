841a5805368a614c538aaf70c70c2eaa7316b241
batukochan <batukochan@icloud.com>
2024-12-06 01:42:20 +0300

    Update: Add lazy to img tags

--- src/components/Information/information.tsx
+++ src/components/Information/information.tsx
@@ -24,7 +24,7 @@ const Information: React.FC<InformationProps> = () => {
       <Tag> {t("about")}</Tag>
       <InformationItem>
         <ImageWrapper>
-          <img src={InformationPhoto} alt="Personal" />
+          <img src={InformationPhoto} alt="Personal" loading="lazy"/>
         </ImageWrapper>
         <ContentWrapper>
           <Text
